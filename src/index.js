c28d1370027c4c727ff022d46ff509f1a555a20f
Pedro Marques Fr <pedromarques1011@gmail.com>
2021-03-30 16:47:06 -0300

    adicionado express rescue

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-var cors = require('cors')
+const express = require("express");
+var cors = require("cors");
+const rescue = require("express-rescue");
 
-const router = require('./routes');
+const router = require("./routes");
 
 const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 app.use((req, res, next) => {
   console.log(`- ${req.method} ${req.path}`);
@@ -17,14 +18,18 @@ app.use((req, res, next) => {
 });
 
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (request, response) => {
+app.get("/", (request, response) => {
   response.send();
 });
 
 app.use(router);
 
-app.use((err, req, res, next) => {
-  res.status(500).send({ error: `${err} ou algum erro interno` });
-});
+app.use(
+  rescue(async (err, req, res, next) => {
+    res.status(500).send({ error: `${err} ou algum erro interno` });
+  })
+);
 
-app.listen(process.env.PORT, () => console.log(`listen port: ${process.env.PORT}`));
+app.listen(process.env.PORT, () =>
+  console.log(`listen port: ${process.env.PORT}`)
+);

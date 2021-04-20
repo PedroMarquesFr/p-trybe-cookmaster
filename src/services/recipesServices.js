83b31160cdbb3cd65b6aa2860319bb2217e106eb
Pedro Marques Freire <pedromarques1011@gmail.com>
2021-04-20 15:09:31 -0300

    mais errors do lint corrigidos

--- src/services/recipesServices.js
+++ src/services/recipesServices.js
@@ -2,7 +2,9 @@ const errMessage = require('./errMessage');
 const recipesModel = require('../models/recipesModel');
 
 const newRecipe = async (recipeName, ingredients, preparation, userId) => {
-  if (!recipeName || !ingredients || !preparation) return errMessage('Invalid entries. Try again.', 400);
+  if (!recipeName || !ingredients || !preparation) {
+    return errMessage('Invalid entries. Try again.', 400);
+  }
   const resp = await recipesModel.createRecipe(
     recipeName,
     ingredients,
@@ -16,11 +18,8 @@ const newRecipe = async (recipeName, ingredients, preparation, userId) => {
 };
 
 const editRecipe = async (userInfo, recipeInfo) => {
-  const doesRecipeWithIdExists = await recipesModel.listRecipesById(
-    recipeInfo.id,
-  );
+  const doesRecipeWithIdExists = await recipesModel.listRecipesById(recipeInfo.id);
   if (!doesRecipeWithIdExists) return errMessage('Id da receita invalido', 401);
-  console.log(userInfo.role, userInfo._id, doesRecipeWithIdExists.userId);
   if (
     userInfo.role === 'admin'
     || userInfo._id.toString() === doesRecipeWithIdExists.userId.toString()
